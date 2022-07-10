9a42ab2a0ffdeca4f95ce8eb1a8ed34db680df61
Wes Hardaker <opensource@hardakers.net>
2022-07-09 20:41:25 -0700

    support a config file for changes to be made

--- nb2an/tools/update_ansible.py
+++ nb2an/tools/update_ansible.py
@@ -7,9 +7,11 @@ from logging import debug, info, warning, error, critical
 import logging
 import sys
 import os
-import nb2an.netbox
-
+import yaml
 import ruamel.yaml
+
+import nb2an.netbox
+import nb2an.dotnest
 
 def parse_args():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter,
@@ -28,6 +30,9 @@ def parse_args():
     parser.add_argument("-d", "--ansible-directory",
                         type=str, help="The ansible directory to verify")
 
+    parser.add_argument("-c", "--changes-file", default=None, type=FileType("r"),
+                        help="The changes definition file to use")
+
     args = parser.parse_args()
     log_level = args.log_level.upper()
     logging.basicConfig(level=log_level,
@@ -35,19 +40,49 @@ def parse_args():
     return args
 
 
-def process_host(b: nb2an.netbox.Netbox, hostname: str, yaml_file: str, outlets: list,
-                 make_changes: bool = True):
+def process_changes(changes, yaml_struct, nb_data):
+    dn = nb2an.dotnest.DotNest(nb_data)
+    for item in changes:
+        if isinstance(changes[item], dict):
+            if item not in yaml_struct:
+                yaml_struct[item] = {}  # TODO: allow list creation
+            process_changes(changes[item], yaml_struct[item], nb_data)
+        elif isinstance(changes[item], str):
+            value = dn.get(changes[item])
+            info(f"got here: {item} {value}")
+            yaml_struct[item] = value
+
+
+def process_host(nb: nb2an.netbox.Netbox, hostname: str, yaml_file: str, outlets: list,
+                 changes: dict = None):
     debug(f"modifying {yaml_file}")
+
+    # load the original YAML
     with open(yaml_file) as original:
         yaml_data = original.read()
 
-        yaml = ruamel.yaml.YAML()
-        yaml.indent(mapping=2, sequence=4, offset=2)
-        yaml.preserve_quotes = True
-        yaml_struct = yaml.load(yaml_data)
+        yaml_parser = ruamel.yaml.YAML()
+        yaml_parser.indent(mapping=2, sequence=4, offset=2)
+        yaml_parser.preserve_quotes = True
+        yaml_struct = yaml_parser.load(yaml_data)
 
+    if changes:
+        debug(changes)
+
+        nb_data = nb.get_devices_by_name(hostname)
+        if not nb_data or len(nb_data) != 1:
+            info("not processing changes for {hostname} as no netbox data found")
+        else:
+            nb_data = nb_data[0]
+            process_changes(changes, yaml_struct, nb_data)
+
+        for item in changes:
+            debug(f"setting: {item} to {changes[item]}")
+
+
+    # write the YAML back out
     with open(yaml_file, "w") as modified:
-        yaml.dump(yaml_struct, modified)
+        yaml_parser.dump(yaml_struct, modified)
 
 
 def main():
@@ -66,13 +101,17 @@ def main():
     devices = nb.get_devices(args.racks)
     outlets = nb.get_outlets()
 
+    changes = None
+    if args.changes_file:
+        changes = yaml.safe_load(args.changes_file.read())
+
     for device in devices:
         name = nb.fqdn(device['name'])
         debug(f"starting: {name}")
 
         device_yaml = os.path.join(ansible_directory, "host_vars", name + ".yml")
         if os.path.exists(device_yaml):
-            process_host(nb, name, device_yaml, outlets, make_changes=(not args.noop))
+            process_host(nb, name, device_yaml, outlets, changes=changes)
 
 
 if __name__ == "__main__":
