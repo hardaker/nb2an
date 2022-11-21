dbd90dbdb6ce175f88e99720ff4a9668dce2d191
Wes Hardaker <opensource@hardakers.net>
2022-11-21 14:43:12 -0800

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nb2an",
-    version="0.9.7",
+    version="0.9.8",
     author="Wes Hardaker",
     author_email="opensource@hardakers.net",
     description="A set of python scripts to compare or update NetBox and Ansible",
